1b86b8cc8d4005fc372e4780d07221df096b2d8f
nebarf <francesco.benedetto263@gmail.com>
2024-03-30 18:03:44 +0100

    chore: create docker image for order service db

---
+++ .docker/order-db/initdb.sql
@@ -0,0 +1,33 @@
+-- Create the schema that we'll use to populate data and watch the effect in the WAL
+CREATE SCHEMA orders;
+SET search_path TO orders;
+
+CREATE TABLE purchase_order (
+	id int8 NOT NULL,
+	customer_id int8 NOT NULL,
+	order_date timestamp(6) NULL,
+	CONSTRAINT purchase_order__pk PRIMARY KEY (id)
+);
+
+CREATE TABLE order_line (
+	id int8 NOT NULL,
+	quantity int4 NOT NULL,
+	total_price numeric(38, 2) NULL,
+	order_id int8 NULL,
+	item varchar(255) NULL,
+	"status" varchar(255) NULL,
+	CONSTRAINT order_line__pk PRIMARY KEY (id),
+	CONSTRAINT order_line__status__enum_check CHECK (((status)::text = ANY ((ARRAY['ENTERED'::character varying, 'CANCELLED'::character varying, 'SHIPPED'::character varying])::text[])))
+);
+ALTER TABLE order_line ADD CONSTRAINT order_line__purchase_order__fk FOREIGN KEY (order_id) REFERENCES purchase_order(id);
+
+CREATE TABLE outbox_event (
+	id uuid NOT NULL,
+	"timestamp" timestamptz(6) NOT NULL,
+	aggregate_id varchar(255) NOT NULL,
+	aggregate_type varchar(255) NOT NULL,
+	payload jsonb NOT NULL,
+	"type" varchar(255) NOT NULL,
+	CONSTRAINT outbox_event__pk PRIMARY KEY (id)
+);
+ALTER TABLE outbox_event REPLICA IDENTITY FULL;
