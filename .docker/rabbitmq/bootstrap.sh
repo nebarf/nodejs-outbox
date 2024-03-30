95d1b37729785f0ca66a5eb266ec54c700b61f6a
nebarf <francesco.benedetto263@gmail.com>
2024-03-30 17:47:40 +0100

    chore: use rabbitmq as message broker forking std docker image

---
+++ .docker/rabbitmq/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Create RabbitMQ resources.
+( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
+rabbitmqctl add_user $RABBITMQ_ADMIN_USER $RABBITMQ_ADMIN_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_ADMIN_USER administrator ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_ADMIN_USER  ".*" ".*" ".*" ; \
+rabbitmqadmin declare exchange name=order.events type=fanout -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; \
+rabbitmqadmin declare queue name=order.events durable=true -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; \
+rabbitmqadmin declare binding source="order.events" destination_type="queue" destination="order.events" -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; ) &
+
+# $@ is used to pass arguments to the rabbitmq-server command.
+# For example if you use it like this: docker run -d rabbitmq arg1 arg2,
+# it will be as you run in the container rabbitmq-server arg1 arg2.
+rabbitmq-server $@
