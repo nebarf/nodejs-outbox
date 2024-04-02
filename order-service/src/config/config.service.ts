6743052f89e4de38e5f21454312b733e08f44929
nebarf <francesco.benedetto263@gmail.com>
2024-04-02 19:54:54 +0200

    refactor(order-service): use config service in main file

--- order-service/src/config/config.service.ts
+++ order-service/src/config/config.service.ts
@@ -1,14 +1,22 @@
 import { Inject } from '@nestjs/common';
 import dbConfig from './database.config';
+import srvConfig from './server.config';
 import { ConfigType } from '@nestjs/config';
 
 export class ConfigService {
   constructor(
     @Inject(dbConfig.KEY)
     private readonly databaseConfig: ConfigType<typeof dbConfig>,
+
+    @Inject(srvConfig.KEY)
+    private readonly serverConfig: ConfigType<typeof srvConfig>,
   ) {}
 
   get database() {
     return this.databaseConfig;
+  }
+
+  get server() {
+    return this.serverConfig;
   }
 }
--- order-service/src/main.ts
+++ order-service/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
-  await app.listen(process.env.SERVER_PORT ?? 3000);
+  const config = app.get(ConfigService);
+
+  await app.listen(config.server.port);
 
   const appUrl = await app.getUrl();
   Logger.log(`Server listening on ${appUrl}`, 'Order');
