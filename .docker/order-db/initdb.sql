aea7d35356aae248910d456264e10cec2cf33f51
nebarf <francesco.benedetto263@gmail.com>
2024-04-02 18:38:09 +0200

    feat(order-db): narrow total_price precision

--- .docker/order-db/initdb.sql
+++ .docker/order-db/initdb.sql
@@ -5,14 +5,14 @@ SET search_path TO orders;
 CREATE TABLE purchase_order (
 	id int8 NOT NULL,
 	customer_id int8 NOT NULL,
-	order_date timestamp(6) NULL,
+	order_date timestamp NULL,
 	CONSTRAINT purchase_order__pk PRIMARY KEY (id)
 );
 
 CREATE TABLE order_line (
 	id int8 NOT NULL,
 	quantity int4 NOT NULL,
-	total_price numeric(38, 2) NULL,
+	total_price decimal(12, 2) NULL,
 	order_id int8 NULL,
 	item varchar(255) NULL,
 	"status" varchar(255) NULL,
@@ -23,7 +23,7 @@ ALTER TABLE order_line ADD CONSTRAINT order_line__purchase_order__fk FOREIGN KEY
 
 CREATE TABLE outbox_event (
 	id uuid NOT NULL,
-	"timestamp" timestamptz(6) NOT NULL,
+	"timestamp" timestamp NOT NULL,
 	aggregate_id varchar(255) NOT NULL,
 	aggregate_type varchar(255) NOT NULL,
 	payload jsonb NOT NULL,
