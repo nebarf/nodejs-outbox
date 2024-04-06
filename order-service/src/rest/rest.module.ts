4f46844194f6c130241041be6f6c011ec1f97e8b
nebarf <francesco.benedetto263@gmail.com>
2024-04-06 15:28:14 +0200

    feat(order-service): collect mapping work in dedicated service

--- order-service/src/rest/rest.module.ts
+++ order-service/src/rest/rest.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { ServicesModule } from 'src/service/services.module';
+import { OrderMapperService } from './order-mapper.service';
 
 @Module({
   imports: [ServicesModule],
   controllers: [OrderController],
+  providers: [OrderMapperService],
 })
 export class RestModule {}
