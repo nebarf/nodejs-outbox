027637ef817190e0d39ab3138f3fb2261be52e81
nebarf <francesco.benedetto263@gmail.com>
2024-04-06 19:06:02 +0200

    chore(shipment-service): create Dockerfile and service in docker compose

--- .docker/shipment-db/initdb.sql
+++ .docker/shipment-db/initdb.sql
@@ -4,15 +4,15 @@ SET search_path TO shipments;
 
 CREATE TABLE consumed_message (
 	event_id uuid NOT NULL,
-	time_of_receiving timestamptz(6) NULL,
+	time_of_receiving timestamp NULL,
 	CONSTRAINT consumed_message__pk PRIMARY KEY (event_id)
 );
 
 CREATE TABLE shipment (
 	id bigserial NOT NULL,
-	customer_id int8 NOT NULL,
-	order_date timestamp(6) NULL,
-	order_id int8 NOT NULL,
+	customer_id integer NOT NULL,
+	order_date timestamp NULL,
+	order_id integer NOT NULL,
 	CONSTRAINT shipment__pk PRIMARY KEY (id)
 );
 
