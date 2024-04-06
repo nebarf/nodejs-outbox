2fba73cecaf55572f56d85a1e28f72ed6caa96d5
nebarf <francesco.benedetto263@gmail.com>
2024-04-06 15:24:24 +0200

    fix(order-service): order created event should carry purchase order

--- order-service/src/event/order-created.event.ts
+++ order-service/src/event/order-created.event.ts
@@ -1,3 +1,11 @@
+import { PurchaseOrder } from 'src/model/purchase-order.entity';
+
 export const OrderCreatedSymbol = Symbol('OrderCreated');
 
-export class OrderCreatedEvent {}
+export class OrderCreatedEvent {
+  readonly order: PurchaseOrder;
+
+  constructor(order: PurchaseOrder) {
+    this.order = order;
+  }
+}
