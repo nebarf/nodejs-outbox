b19f54eb61e6b27e435d69f0c93af1bf7a84e31c
nebarf <francesco.benedetto263@gmail.com>
2024-04-15 19:07:40 +0200

    feat(shipment-service): hook database module

--- apps/shipment-service/src/main.module.ts
+++ apps/shipment-service/src/main.module.ts
@@ -1,8 +1,36 @@
 import { HealthModule } from '@libs/health';
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigService } from './config/config.service';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+
+const logger = new Logger('Database');
 
 @Module({
-  imports: [HealthModule, ConfigModule],
+  imports: [
+    HealthModule,
+    ConfigModule,
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        entities: ['./dist/apps/shipment-service/src/model'],
+        entitiesTs: ['./src/model'],
+        forceUtcTimezone: true,
+        driver: PostgreSqlDriver,
+        host: config.database.host,
+        dbName: config.database.name,
+        schema: config.database.schema,
+        port: config.database.port,
+        user: config.database.user,
+        password: config.database.password,
+        debug: config.database.debug,
+        logger: logger.log.bind(logger),
+        highlighter: new SqlHighlighter(),
+      }),
+    }),
+  ],
 })
 export class MainModule {}
