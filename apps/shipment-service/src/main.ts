1c852acc3a5af38ec0d48b3660b25e4104f3422f
nebarf <francesco.benedetto263@gmail.com>
2024-04-15 10:01:31 +0200

    fix(shipment-service): get app url from app

--- apps/shipment-service/src/main.ts
+++ apps/shipment-service/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './main.module';
 import { ConfigService } from './config/config.service';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
   const config = app.get(ConfigService);
 
-  const appUrl = await app.listen(config.server.port);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(config.server.port);
+
+  const appUrl = await app.getUrl();
   Logger.log(`Server listening on ${appUrl}`, 'Shipment');
 }
 bootstrap();
