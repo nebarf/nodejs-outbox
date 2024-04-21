cb5d1ffdcb76cb48e1fe10e0edf4cd13d032b81d
nebarf <francesco.benedetto263@gmail.com>
2024-04-21 19:02:43 +0200

    fix(shipment-service): import missing exported-events module

--- apps/shipment-service/src/event/events.module.ts
+++ apps/shipment-service/src/event/events.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RabbitMqConsumerService } from './rabbitmq-consumer.service';
 import { ServicesModule } from '../service/services.module';
 import { ConfigModule } from '../config/config.module';
+import { ExportedEventsModule } from '@libs/events';
 
 @Module({
-  imports: [ServicesModule, ConfigModule],
+  imports: [ServicesModule, ConfigModule, ExportedEventsModule],
   providers: [RabbitMqConsumerService],
 })
 export class EventsModule {}
