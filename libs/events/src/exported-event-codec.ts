7e2ef6b6b8c31e34fc786128b3785ed993583dde
nebarf <francesco.benedetto263@gmail.com>
2024-04-21 17:57:48 +0200

    feat(events-lib): allow to parse event from string

--- libs/events/src/exported-event-codec.ts
+++ libs/events/src/exported-event-codec.ts
@@ -1,48 +1,52 @@
 import { validateSync } from 'class-validator';
-import { EventType } from './event-type';
-import { ExportedEvent } from './exported-event';
 import {
   ClassConstructor,
   instanceToPlain,
   plainToInstance,
 } from 'class-transformer';
-import { serialize } from 'node:v8';
+import { EventType } from './event-type';
+import { ExportedEvent } from './exported-event';
 
-export type ParseResult<T extends EventType, R extends ExportedEvent<T>> =
-  | { _tag: 'success'; instance: R }
-  | { _tag: 'failure'; error: string };
+export type ParseResult<
+  T extends EventType,
+  R extends ExportedEvent<T>,
+  E extends Error = Error,
+> = { _tag: 'success'; instance: R } | { _tag: 'failure'; error: E };
 
 export type ParseResultSuccess<
   T extends EventType,
   R extends ExportedEvent<T>,
 > = Extract<ParseResult<T, R>, { _tag: 'success' }>;
+
 export type ParseResultFailure<
   T extends EventType,
   R extends ExportedEvent<T>,
 > = Extract<ParseResult<T, R>, { _tag: 'failure' }>;
 
 export class ExportedEventCodec {
-  toPlain<T extends EventType>(event: ExportedEvent<T>) {
-    return instanceToPlain(event);
-  }
-
-  toBuffer<T extends EventType>(event: ExportedEvent<T>) {
-    return serialize(this.toPlain(event));
-  }
-
-  toString<T extends EventType>(event: ExportedEvent<T>) {
-    return this.toBuffer(event).toString();
-  }
-
   parse<T extends EventType, R extends ExportedEvent<T>>(
-    event: unknown,
+    event: Record<string, unknown> | string,
     ctor: ClassConstructor<R>,
   ): ParseResult<T, R> {
-    const instance = plainToInstance(ctor, event);
+    let plain: Record<string, unknown>;
+    if (typeof event === 'string') {
+      try {
+        plain = JSON.parse(event);
+      } catch (err) {
+        return {
+          _tag: 'failure' as const,
+          error: new Error('Failed to parse event from string'),
+        };
+      }
+    } else {
+      plain = event;
+    }
+
+    const instance = plainToInstance(ctor, plain);
 
     const validationErrors = validateSync(instance);
     if (validationErrors.length > 0) {
-      return { _tag: 'failure', error: validationErrors.toString() };
+      return { _tag: 'failure', error: new Error(validationErrors.toString()) };
     }
 
     return { _tag: 'success', instance };
@@ -62,5 +66,14 @@ export class ExportedEventCodec {
 
   serialize<T extends EventType>(event: ExportedEvent<T>) {
     return this.toString(event);
+  }
+
+  toString<T extends EventType>(event: ExportedEvent<T>) {
+    const plain = instanceToPlain(event);
+    return JSON.stringify(plain);
+  }
+
+  toPlain<T extends EventType>(event: ExportedEvent<T>) {
+    return instanceToPlain(event);
   }
 }
