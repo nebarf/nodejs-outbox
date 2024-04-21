9d72de482bbb63bcaf149230ae185da76aac432c
nebarf <francesco.benedetto263@gmail.com>
2024-04-21 18:05:58 +0200

    feat(shipment-service): implement missing methods of message-log service

--- apps/shipment-service/src/service/message-log.service.ts
+++ apps/shipment-service/src/service/message-log.service.ts
@@ -1,4 +1,23 @@
+import { EntityManager } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
+import { UUID } from 'crypto';
+import { ConsumedMessage } from '../model/consumed-message.entity';
 
 @Injectable()
-export class MessageLogService {}
+export class MessageLogService {
+  constructor(private readonly entityManager: EntityManager) {}
+
+  markAsProcessed(eventId: UUID) {
+    const message = new ConsumedMessage();
+    message.eventId = eventId;
+
+    this.entityManager.persist(message);
+  }
+
+  async wasAlreadyProcessed(eventId: UUID) {
+    const message = await this.entityManager.findOne(ConsumedMessage, {
+      eventId,
+    });
+    return !!message;
+  }
+}
