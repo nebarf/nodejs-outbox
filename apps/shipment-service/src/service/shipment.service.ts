db1717ae31f7028047d51d7bb11c2848bba05d98
nebarf <francesco.benedetto263@gmail.com>
2024-04-21 18:06:24 +0200

    feat(shipment-service): handle order events through shipment service

--- apps/shipment-service/src/service/shipment.service.ts
+++ apps/shipment-service/src/service/shipment.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { OrderCreatedExportedEvent } from '@libs/events/order-created-event';
+import { OrderLineUpdatedExportedEvent } from '@libs/events/order-line-updated.event';
+import { Injectable, Logger } from '@nestjs/common';
+import { Shipment } from '../model/shipment.entity';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 @Injectable()
-export class ShipmentService {}
+export class ShipmentService {
+  private readonly logger = new Logger('ServicesModule');
+
+  constructor(private readonly entityManager: EntityManager) {}
+
+  orderCreated(event: OrderCreatedExportedEvent) {
+    const shipment = new Shipment();
+
+    shipment.customerId = event.customerId;
+    shipment.orderDate = event.orderDate;
+    shipment.orderId = event.id;
+
+    this.entityManager.persist(shipment);
+  }
+
+  orderLineUpdated(event: OrderLineUpdatedExportedEvent) {
+    this.logger.warn(`Processing of ${event.eventType} not yet implemented`);
+  }
+}
