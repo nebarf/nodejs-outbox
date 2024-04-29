5772fd47e20ac174c4e27973382178d8747befcd
nebarf <francesco.benedetto263@gmail.com>
2024-04-29 17:18:13 +0200

    chore(shipment): change name of order queue

--- .docker/rabbitmq/bootstrap.sh
+++ .docker/rabbitmq/bootstrap.sh
@@ -6,8 +6,8 @@ rabbitmqctl add_user $RABBITMQ_ADMIN_USER $RABBITMQ_ADMIN_PASSWORD 2>/dev/null ;
 rabbitmqctl set_user_tags $RABBITMQ_ADMIN_USER administrator ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_ADMIN_USER  ".*" ".*" ".*" ; \
 rabbitmqadmin declare exchange name=order.events type=fanout -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; \
-rabbitmqadmin declare queue name=order.events durable=true -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; \
-rabbitmqadmin declare binding source="order.events" destination_type="queue" destination="order.events" -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; ) &
+rabbitmqadmin declare queue name=shipment.orders durable=true -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; \
+rabbitmqadmin declare binding source="order.events" destination_type="queue" destination="shipment.orders" -u $RABBITMQ_ADMIN_USER -p $RABBITMQ_ADMIN_PASSWORD ; ) &
 
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
